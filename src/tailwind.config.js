57cc96a1572d2c0da4200e22fb13dbcfa863d53e
DPolar05 <dgomezc1301@alumno.ipn.mx>
2025-04-23 06:28:21 -0600

    Fix: ajustado content para detectar todas las rutas en src/

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "class",
-	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "*.{js,ts,jsx,tsx,mdx}"],
+	content: [
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	  "./app/**/*.{js,ts,jsx,tsx}",
+	  "./components/**/*.{js,ts,jsx,tsx}",
+	  "./pages/**/*.{js,ts,jsx,tsx}",
+	  "./public/**/*.{html,js}",
+	],
 	theme: {
 	  container: {
 		center: true,
